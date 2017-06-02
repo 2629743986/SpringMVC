7a722c871cbb87d08395cb03c3ac297fe1d2ab1f
2629743986 <2629743986@qq.com>
2017-06-02 12:56:10 +0800

    测试一下怎么使用 pull request

---
+++ src/main/java/com/wangjie/spring/kaijie/Kaijie.java
@@ -0,0 +1,10 @@
+package com.wangjie.spring.kaijie;
+
+/**
+ * Created by 26297 on 2017/6/2.
+ */
+public class Kaijie {
+    public static void main(String[] args){
+        System.out.println("Just test something interesting!!");
+    }
+}
